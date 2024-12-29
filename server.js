ec93fa15c792c3917442ddc7072108ff401b60e4
Nmc10 <nico.c10.ale@gmail.com>
2024-12-29 00:20:14 -0300

    Se realizan ultimos ajustes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config(); // Cargar las variables de entorno desde el archivo .env
 
 const app = express();
 const PORT = process.env.PORT || 3002;
-
+app.use(cors());
 app.use(express.static('public'));
 
 // Ciudades para mostrar el clima
